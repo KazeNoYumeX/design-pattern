71a2437965a8bc4418a39c17222fddc003f705ba
KazeNoYumeX <foever527@gmail.com>
2024-02-16 02:15:20 +0800

    Feat: Added C2MB Boss - BigTwo

---
+++ C2MB/src/Players/HumanPlayer.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace C2MB\Players;
+
+use C1M1H1\Card;
+use C1M1H1\ExchangeHands;
+
+class HumanPlayer extends Player
+{
+    public function chooseAction(): void
+    {
+        $players = $this->showdown->getPlayers();
+
+        // If there are more than one player and exchange permission then choose action
+        if (count($players) > 1 && $this->exchangePermission) {
+            // choose action 1. 換牌 2. 出牌
+            echo "{$this->getName()} 選擇行動: 1. 換牌 2. 出牌\n";
+            $action = trim(fgets(STDIN));
+            if ($action == 1) {
+                // exchange card and set exchange permission to false
+                echo '請選擇要與哪位玩家交換手牌: ';
+
+                // echo players list and filter out current player
+                foreach ($players as $index => $player) {
+                    if ($player->getName() != $this->getName()) {
+                        echo "[$index] {$player->getName()} ";
+                    }
+                }
+                echo "\n";
+
+                $target = trim(fgets(STDIN));
+
+                // If target is empty or target is not in players list then choose action again
+                if (empty($players[$target]) || $players[$target]->getName() == $this->getName()) {
+                    echo "輸入錯誤, 請重新輸入\n";
+                    $this->chooseAction();
+
+                    return;
+                }
+
+                // Exchange card
+                $exchangeHand = new ExchangeHands($this, $players[$target]);
+                $this->showdown->addExchangeHands($exchangeHand);
+                $this->setExchangePermission(false);
+            } elseif ($action != 2) {
+                echo "輸入錯誤, 請重新輸入\n";
+                $this->chooseAction();
+
+                return;
+            }
+        }
+
+        $targetPlayer = $this->checkChangeHands();
+        echo "{$targetPlayer->getName()} 的手牌: ";
+        foreach ($targetPlayer->getHand() as $index => $card) {
+            /** @var Card $card */
+            echo "[$index] {$card->getSuit()->toCardString()}{$card->getRank()->toCardString()} ";
+        }
+        echo "\n請選擇要出的牌: ";
+
+        $targetCard = trim(fgets(STDIN));
+
+        if (empty($targetPlayer->hand->cards[$targetCard])) {
+            echo "輸入錯誤, 請重新輸入\n";
+            $this->chooseAction();
+        } else {
+            // Show card and
+            $card = $targetPlayer->hand->cards[$targetCard];
+            $this->showCard($card);
+
+            // Remove card from hand
+            unset($targetPlayer->hand->cards[$targetCard]);
+            $targetPlayer->hand->cards = array_values($this->hand->cards);
+        }
+    }
+
+    public function nameHimself(): void
+    {
+        $name = fgets(STDIN);
+        $name = trim($name);
+        $this->setName($name);
+    }
+
+    public function takeAction(array $actions): int
+    {
+        $action = trim(fgets(STDIN));
+
+        if (! array_key_exists($action, $actions)) {
+            echo "輸入錯誤, 請重新輸入\n";
+            $this->takeAction($actions);
+        }
+
+        return $action;
+    }
+
+    public function generateName(): string
+    {
+        $name = fgets(STDIN);
+
+        return trim($name);
+    }
+}
---
+++ C2MB/src/Round.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace C2MB;
+
+use C2MB\Cards\Card;
+use C2MB\Games\BigTwo;
+use C2MB\Players\Player;
+
+class Round
+{
+    private BigTwo $game;
+
+    private int $num = 0;
+
+    public function init(): void
+    {
+        $game = $this->getGame();
+
+        $deck = $game->deck;
+        echo "開始洗牌\n";
+        $deck->shuffle();
+        echo "開始抽牌\n";
+
+        // Distribute 13 cards to each player
+        $players = $game->getPlayers();
+        $deck->distributeCards($players, 13);
+    }
+
+    public function start(): void
+    {
+        $game = $this->getGame();
+        $players = $game->getPlayers();
+
+        // Plus the turn number and show the turn number
+        $turn = $this->addTurn();
+        echo "第{$turn}回合開始\n";
+
+        // Each player plays to choose an action
+        foreach ($players as $player) {
+            /** @var Player $player */
+            $player->takeTurn();
+        }
+
+        $this->getWinner();
+        $this->end();
+    }
+
+    public function getWinner(): void
+    {
+        $game = $this->getGame();
+        $players = $game->getPlayers();
+        $maxIndex = 0;
+        $max = null;
+
+        foreach ($players as $index => $player) {
+            /** @var Card $card */
+            $card = $player->getShowedCard();
+
+            if (empty($max)) {
+                $max = $card;
+
+                // Pass the first player
+                continue;
+            }
+
+            // Compare card rank and suit
+            if ($card->compare($max)) {
+                $max = $card;
+                $maxIndex = $index;
+            }
+        }
+
+        // If no winner, break the turn
+        if (empty($max)) {
+            echo "本回合無人出牌\n";
+
+            return;
+        }
+
+        $suit = $max->getSuit()->toCardString();
+        $rank = $max->getRank()->toCardString();
+
+        /** @var Player $winner */
+        $winner = $players[$maxIndex];
+        $winner->gainPoint();
+
+        echo "本回合贏家為：{$winner->getName()}, ";
+        echo "目前得分為：{$winner->getPoint()}, ";
+        echo "最大的牌為：$suit $rank \n";
+    }
+
+    public function end(): void
+    {
+        $num = $this->getNum();
+        echo "第{$num}回合結束\n";
+
+        $game = $this->getGame();
+        if ($game->endCondition()) {
+            $winners = $game->getWinners();
+            $game->end($winners);
+        } else {
+            $this->start();
+        }
+    }
+    public function getGame(): BigTwo
+    {
+        return $this->game;
+    }
+
+    public function setGame(BigTwo $game): void
+    {
+        $this->game = $game;
+    }
+
+    public function addTurn(): int
+    {
+        $turn = $this->getNum() + 1;
+        $this->setNum($turn);
+
+        return $turn;
+    }
+
+    public function getNum(): int
+    {
+        return $this->num;
+    }
+
+    public function setNum(int $num): void
+    {
+        $this->num = $num;
+    }
+}
